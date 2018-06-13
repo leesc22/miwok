f0728fe1f29c347020c21e2670dc13b5d600d5f5
leesc22 <leesiewching22@gmail.com>
2018-06-13 17:12:58 +0800

    Use an intent to open another activity

--- app/src/main/java/com/example/android/miwok/MainActivity.kt
+++ app/src/main/java/com/example/android/miwok/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.android.miwok
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun openNumbersList (view: View) {
+        // Create an Intent to start the second activity
+        val numberIntent = Intent(this, NumbersActivity::class.java)
+
+        // Start the new activity
+        startActivity(numberIntent)
     }
 }
