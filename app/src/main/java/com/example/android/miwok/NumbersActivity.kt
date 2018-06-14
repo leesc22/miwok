6ffa241484074f3c0130dff0cb8c76e3710d12bb
leesc22 <leesiewching22@gmail.com>
2018-06-15 07:11:21 +0800

    Switch from Array to ArrayList

--- app/src/main/java/com/example/android/miwok/NumbersActivity.kt
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.kt
@@ -11,19 +11,19 @@ class NumbersActivity : AppCompatActivity() {
         setContentView(R.layout.activity_numbers)
 
         // Create an array of words
-        val words = arrayOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten")
+        val words = arrayListOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten")
 
         // Verify the contents of the array by printing out each array element to the logs
-        Log.v("NumbersActivity", "Word at index 0: " + words[0])
-        Log.v("NumbersActivity", "Word at index 1: " + words[1])
-        Log.v("NumbersActivity", "Word at index 2: " + words[2])
-        Log.v("NumbersActivity", "Word at index 3: " + words[3])
-        Log.v("NumbersActivity", "Word at index 4: " + words[4])
-        Log.v("NumbersActivity", "Word at index 5: " + words[5])
-        Log.v("NumbersActivity", "Word at index 6: " + words[6])
-        Log.v("NumbersActivity", "Word at index 7: " + words[7])
-        Log.v("NumbersActivity", "Word at index 8: " + words[8])
-        Log.v("NumbersActivity", "Word at index 9: " + words[9])
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(0))
+        Log.v("NumbersActivity", "Word at index 1: " + words.get(1))
+        Log.v("NumbersActivity", "Word at index 2: " + words.get(2))
+        Log.v("NumbersActivity", "Word at index 3: " + words.get(3))
+        Log.v("NumbersActivity", "Word at index 4: " + words.get(4))
+        Log.v("NumbersActivity", "Word at index 5: " + words.get(5))
+        Log.v("NumbersActivity", "Word at index 6: " + words.get(6))
+        Log.v("NumbersActivity", "Word at index 7: " + words.get(7))
+        Log.v("NumbersActivity", "Word at index 8: " + words.get(8))
+        Log.v("NumbersActivity", "Word at index 9: " + words.get(9))
 
     }
 }
