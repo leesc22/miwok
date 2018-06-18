240792d93884dd9f02e92174c42e1195f3147e4e
leesc22 <leesiewching22@gmail.com>
2018-06-18 13:49:43 +0800

    Return code back to using ListView

--- app/src/main/java/com/example/android/miwok/NumbersActivity.kt
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.kt
@@ -3,7 +3,7 @@ package com.example.android.miwok
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import android.widget.GridView
+import android.widget.ListView
 
 
 class NumbersActivity : AppCompatActivity() {
@@ -25,12 +25,12 @@ class NumbersActivity : AppCompatActivity() {
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
         // activity_numbers.xml layout file.
-        val gridView = findViewById(R.id.gridview) as GridView
+        val listView = findViewById(R.id.list) as ListView
 
         // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
         // {@link ListView} will display list items for each word in the list of words.
         // Do this by calling the setAdapter method on the {@link ListView} object and pass in
         // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
-        gridView.setAdapter(itemsAdapter)
+        listView.setAdapter(itemsAdapter)
     }
 }
