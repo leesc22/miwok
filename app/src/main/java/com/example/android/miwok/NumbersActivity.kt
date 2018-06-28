b688a099eff4322e20e9ed5692a588151cfe489b
leesc22 <leesiewching22@gmail.com>
2018-06-28 23:10:25 +0800

    Cleaning Up Media Resources

--- app/src/main/java/com/example/android/miwok/NumbersActivity.kt
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.kt
@@ -9,7 +9,16 @@ import kotlinx.android.synthetic.main.word_list.*
 class NumbersActivity : AppCompatActivity() {
 
     /** Handles playback of all the sound files */
-    private lateinit var mediaPlayer: MediaPlayer
+    private var mediaPlayer: MediaPlayer? = null
+
+    /**
+     * This listener gets triggered when the {@link MediaPlayer} has completed
+     * playing the audio file.
+     */
+    private val completionListener = MediaPlayer.OnCompletionListener {
+        // Now that the sound file has finished playing, release the media player resources.
+        releaseMediaPlayer()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +44,10 @@ class NumbersActivity : AppCompatActivity() {
 
         // Set a click listener to play the audio when the list item is clicked on
         list.setOnItemClickListener { adapterView, view, i, l ->
+            // Release the media player if it currently exists because we are about to
+            // play a different sound file
+            releaseMediaPlayer()
+
             // Get the {@link Word} object at the given position the user clicked on
             val word: Word = words.get(i)
 
@@ -43,7 +56,28 @@ class NumbersActivity : AppCompatActivity() {
             mediaPlayer = MediaPlayer.create(this, word.audioResourceId)
 
             // Start the audio file
-            mediaPlayer.start()
+            mediaPlayer?.start()
+
+            // Setup a listener on the media player, so that we can stop and release the
+            // media player once the sound has finished playing.
+            mediaPlayer?.setOnCompletionListener(completionListener)
+        }
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private fun releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mediaPlayer?.release()
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mediaPlayer = null
         }
     }
 }
