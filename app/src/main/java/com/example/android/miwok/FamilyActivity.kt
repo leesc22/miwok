b54c3d0a5a31b5fd73c54f2a2b3d486d637186ed
leesc22 <leesiewching22@gmail.com>
2018-07-01 12:59:10 +0800

    Manage audio focus

--- app/src/main/java/com/example/android/miwok/FamilyActivity.kt
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.kt
@@ -1,6 +1,12 @@
 package com.example.android.miwok
 
+import android.annotation.TargetApi
+import android.content.Context
+import android.media.AudioAttributes
+import android.media.AudioFocusRequest
+import android.media.AudioManager
 import android.media.MediaPlayer
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.word_list.*
@@ -9,6 +15,35 @@ class FamilyActivity : AppCompatActivity() {
 
     /** Handles playback of all the sound files */
     private var mediaPlayer: MediaPlayer? = null
+
+    /** Handles audio focus when playing a sound file */
+    private var audioManager: AudioManager? = null
+    private var focusRequest: AudioFocusRequest? = null
+
+    /**
+     * This listener gets triggered whenever the audio focus changes
+     * (i.e., we gain or lose audio focus because of another app or device).
+     */
+    private var audioFocusChangeListener = AudioManager.OnAudioFocusChangeListener { focusChange ->
+        if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT || focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+            // The AUDIOFOCUS_LOSS_TRANSIENT case means that we've lost audio focus for a
+            // short amount of time. The AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK case means that
+            // our app is allowed to continue playing sound but at a lower volume. We'll treat
+            // both cses the same way because our app is playing short sound files.
+
+            // Pause playback and reset player to the start of the file. That way, we can
+            // play the word from the beginning when we resume playback.
+            mediaPlayer?.pause()
+            mediaPlayer?.seekTo(0)
+        } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+            // The AUDIOFOCUS_GAIN case means we have regained focus and can resume playback.
+            mediaPlayer?.start()
+        } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+            // The AUDIOFOCUS_LOSS case means we've lost audio focus and
+            // Stop playback and clean up resources
+            releaseMediaPlayer()
+        }
+    }
 
     /**
      * This listener gets triggered when the {@link MediaPlayer} has completed
@@ -22,6 +57,9 @@ class FamilyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.word_list)
+
+        // Create and setup the {@link AudioManager} to request audio focus
+        audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
 
         // Create a list of words
         val words = arrayListOf<Word>()
@@ -50,16 +88,40 @@ class FamilyActivity : AppCompatActivity() {
             // Get the {@link Word} object at the given position the user clicked on
             val word: Word = words.get(i)
 
-            // Create and setup the {@link MediaPlayer} for the audio resource associated
-            // with the current word
-            mediaPlayer = MediaPlayer.create(this, word.audioResourceId)
+            // Request audio focus so in order to play the audio file. The app needs to play a
+            // short audio file, so we will request audio focus with a short amount of time
+            // with AUDIOFOCUS_GAIN_TRANSIENT.
+            var result: Int
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val playbackAttributes = AudioAttributes.Builder()
+                        .setUsage(AudioAttributes.USAGE_MEDIA)
+                        .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+                        .build()
+                focusRequest = AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
+                        .setAudioAttributes(playbackAttributes)
+                        .setOnAudioFocusChangeListener(audioFocusChangeListener)
+                        .build()
+                @TargetApi(Build.VERSION_CODES.O)
+                result = audioManager!!.requestAudioFocus(focusRequest)
+            } else {
+                @Suppress("DEPRECATION")
+                result = audioManager!!.requestAudioFocus(audioFocusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT)
+            }
 
-            // Start the audio file
-            mediaPlayer?.start()
+            if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                // We have audio focus now.
 
-            // Setup a listener on the media player, so that we can stop and release the
-            // media player once the sound has finished playing.
-            mediaPlayer?.setOnCompletionListener(completionListener)
+                // Create and setup the {@link MediaPlayer} for the audio resource associated
+                // with the current word
+                mediaPlayer = MediaPlayer.create(this, word.audioResourceId)
+
+                // Start the audio file
+                mediaPlayer?.start()
+
+                // Setup a listener on the media player, so that we can stop and release the
+                // media player once the sound has finished playing.
+                mediaPlayer?.setOnCompletionListener(completionListener)
+            }
         }
     }
 
@@ -84,6 +146,15 @@ class FamilyActivity : AppCompatActivity() {
             // setting the media player to null is an easy way to tell that the media player
             // is not configured to play an audio file at the moment.
             mediaPlayer = null
+
+            // Regardless of whether or not we were granted audio focus, abandon it. This also
+            // unregisters the AudioFocusChangeListener so we don't get anymore callbacks.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                audioManager?.abandonAudioFocusRequest(focusRequest)
+            } else {
+                @Suppress("DEPRECATION")
+                audioManager?.abandonAudioFocus(audioFocusChangeListener)
+            }
         }
     }
 }
